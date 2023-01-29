38146bfa380308901f9fc5b815fb7b19882d396c
Checksum <checksum@velvet.moe>
2023-01-29 15:52:24 -0600

    refactor: cleanup database stuffs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import logging
-from collections import deque
 from os import getenv
 
-from aiosqlite import connect
+import aiosqlite
+from discord import Color, Embed, TextChannel
 from dotenv import load_dotenv
 from nextcord import Activity, ActivityType, Status
 from nextcord.ext import commands
@@ -10,6 +10,10 @@ from nextcord.ext import commands
 from commands import Commands
 from events import Events
 from modules.beatleader import BeatLeader
+import redis.asyncio as redis
+from modules.status_manager import CLOUDFLARE_IMG
+
+from version import VERSION
 
 load_dotenv()
 
@@ -19,11 +23,56 @@ logging.basicConfig(level=logging.INFO)
 class Client(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        # Global Variables
-        self.sl = deque(maxlen=30)
-        self.average_latency = 0
 
+        self.redis = redis.Redis()
+        self.loop.create_task(self.startup())
+
+    async def init_db(self, db):
+        await db.execute("""CREATE TABLE IF NOT EXISTS meta
+                            (version TEXT NOT NULL);""")
+
+        await db.execute("""
+                CREATE TABLE IF NOT EXISTS bot_settings (
+                  name varchar(255) PRIMARY KEY,
+                  value varchar(255)
+                );
+            """)
+
+        await db.commit()
+
+    async def startup(self):
+        await self.wait_until_ready()
+
+        async with aiosqlite.connect('bot.db') as db:
+            await self.init_db(db)
+
+            async with db.execute("SELECT value FROM bot_settings WHERE name='alert_channel'") as cursor:
+                channel_id = await cursor.fetchone()
+
+            # don't update internal version unless we have a channel set for notifying about it.
+            if channel_id is not None:
+                async with db.execute("SELECT version FROM meta") as cursor:
+                    version = await cursor.fetchone()
+
+                    if version:
+                        if version[0] != VERSION:
+                            # If we have updated, update the version in the DB
+                            await db.execute("UPDATE meta SET version = ?", (VERSION,))
+                            await db.commit()
+
+                            # Send an embed into the discord channel
+                            embed = Embed()
+                            embed.color = Color.orange()
+                            embed.set_author(
+                                name=f"The bot has been updated to v{VERSION}.", icon_url=CLOUDFLARE_IMG)
+
+                            channel: TextChannel = self.get_channel(int(channel_id[0]))  # type: ignore
+                            await channel.send(embed=embed)
+                    else:
+                        await db.execute("INSERT INTO meta (version) VALUES (?)", (VERSION,))
+                        await db.commit()
+
+        # Startup complete, init the rest of the bot
         self.add_cog(Events(self))
         self.add_cog(Commands(self))
         self.add_cog(BeatLeader(self))
